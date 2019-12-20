586df34965c5d8bfb3ec5c217bc17d9ffea53be1
Alexandra Servie <alexservie@Alexandras-MacBook-Pro-2.local>
2019-12-20 12:27:55 +0100

    seeding complete

---
+++ models/User.js
@@ -0,0 +1,19 @@
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    age: Number,
+    eyeColor: String,
+    name: String,
+    gender: {
+        type: String,
+        enum: ['male', 'female']
+    },
+    company: String,
+    email: String,
+    phone: String,
+    address: String
+})
+const Data = mongoose.model('Data', userSchema);
+module.exports = Data;
